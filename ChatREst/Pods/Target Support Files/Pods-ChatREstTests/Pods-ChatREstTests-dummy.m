4c0bd40712429aa72ab4168a027a53c331e70d31
Rakesh R <rakeshramesh2000@gmail.com>
2018-04-28 12:05:54 +0530

    added alamofire,Swifty-JSON and Socket.IO-Client-Swift

---
+++ ChatREst/Pods/Target Support Files/Pods-ChatREstTests/Pods-ChatREstTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChatREstTests : NSObject
+@end
+@implementation PodsDummy_Pods_ChatREstTests
+@end
---
+++ ChatREst/Pods/Target Support Files/Pods-ChatREstUITests/Pods-ChatREstUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChatREstUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ChatREstUITests
+@end
